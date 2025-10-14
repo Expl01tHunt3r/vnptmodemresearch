59862778dc9ea59e8a92649a35a1287cf428a718
AppleSang <96937073+AppleSang@users.noreply.github.com>
2025-10-14 22:48:48 +0700

    Update install-ns.sh

--- AdGuard/install-ns.sh
+++ AdGuard/install-ns.sh
@@ -64,6 +64,8 @@ read dummy
 echo -e "Bắt đầu quá trình cài đặt."
 mkdir -p /tmp/userdata/AdGuard
 cd /tmp/userdata/AdGuard
+/userfs/bin/curl -s -k -o AdGuard.sh https://raw.githubusercontent.com/Expl01tHunt3r/vnptmodemresearch/refs/heads/main/AdGuard/AdGuard.sh
+chmod +x AdGuard.sh
 /userfs/bin/curl -s -k -o ca-certificates.crt https://raw.githubusercontent.com/Expl01tHunt3r/vnptmodemresearch/refs/heads/main/AdGuard/ca-certificates.crt
 export SSL_CERT_FILE=/tmp/userdata/AdGuard/ca-certificates.crt
 echo -e "\033[32m[OK]\033[0m Đã tạo thành công thư mục chứa config!"
