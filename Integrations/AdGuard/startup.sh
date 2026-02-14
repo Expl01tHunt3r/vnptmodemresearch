cc281440241f802c4eae60d32365e879eae2ed7b
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-02-14 21:06:52 +0700

    today i know curl have retry

--- Integrations/AdGuard/startup.sh
+++ Integrations/AdGuard/startup.sh
@@ -4,13 +4,12 @@
 # Copyright © 2025-2026 Expl01tHunt3r, collaborators and contributors.
 #
 # Note: Startup script for AdGuardHome
+# Note: !!This script only run when in folder tmp doesn't have AdGuardHome!!
 
 if [ ! -e /tmp/AdGuardHome ]; then
-    sleep 180
     export SSL_CERT_FILE=/tmp/userdata/AdGuard/ca.crt
     cd /tmp/ || exit 1
-    /userfs/bin/curl -s -fSL -o AdG_armv5l.tar.gz \
-    https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_armv5.tar.gz
+    /userfs/bin/curl -s -fSL -o AdG_armv5l.tar.gz --retry 1000 --retry-delay 5 https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_armv5.tar.gz
     tar -xzf AdG_armv5l.tar.gz
     rm AdG_armv5l.tar.gz
     cd AdGuardHome || exit 1
