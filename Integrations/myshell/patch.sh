48f1a9b911a5db3f45e3ce1e7cc387fa1e1a9c87
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-02-21 17:31:02 +0700

    viết những thứ cần viế

--- Integrations/myshell/patch.sh
+++ Integrations/myshell/patch.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-# Autorun.sh
+# myshell.sh
 # Copyright © 2025-2026 Expl01tHunt3r, collaborators and contributors.
 #
 # Note: this exactly a shell script run in terminal
+
+# add some color
+
+# Check req
+
+# show text
+
+# Create 3 file: config.sh // startup.sh // banner.txt in folder myshell
+
+# result
