37ac2dda1d4f88a692ce76249418a30b9804b27f
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-02-23 13:42:44 +0700

    Update install.sh

--- Integrations/btop/install.sh
+++ Integrations/btop/install.sh
@@ -5,11 +5,36 @@
 #
 # Note: this exactly a shell script run in terminal
 
+
+GREEN='\033[32m'
+B_MAGENTA='\033[95m'
+RED='\033[31m'
+RESET='\033[0m'
+YELLOW='\033[33m'
+CYAN='\033[36m'
+# Check req
+if [ ! -d /tmp/userdata/myshell ]; then
+    echo -e "\n\n"
+    echo -e "${YELLOW}!! ${RED}Can't run this patch ${YELLOW}!!"
+    echo -e "${RED}You must read about this for resolve this issue!"
+    echo -e "${GREEN}https://github.com/Expl01tHunt3r/vnptmodemresearch/tree/master/Integrations/myshell#1-y%C3%AAu-c%E1%BA%A7u${RESET}"
+    echo -e "\n\n"
+    exit 1
+fi
+# show text
+echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+echo -e "\033[31;43mScript Make By AppleSang With <3\033[0m\n"
+echo -e "${RED}############################################################\n"
+echo -e "${GREEN} https://github.com/Expl01tHunt3r/vnptmodemresearch\n"
+echo -e "${RED}############################################################\n"
+echo -e "        ${YELLOW}Press enter to confirm for install ${CYAN}btop${RESET}"
+read _
+
 cd /tmp/userdata
-rm -rf btop
-mkdir btop
-/userfs/bin/curl -Lk -o btop/btop https://github.com/Expl01tHunt3r/vnptmodemresearch/raw/refs/heads/master/Integrations/btop/btop
+mkdir -p btop
 cd btop
+mkdir -p config
+/userfs/bin/curl -Lk -o btop https://github.com/Expl01tHunt3r/vnptmodemresearch/raw/refs/heads/master/Integrations/btop/btop
 chmod 777 btop
-echo "Btop have been installed at \"/tmp/userdata/btop/btop\"."
-echo "You can start btop using \"/tmp/userdata/btop/btop --force-utf\""
+echo -e "${GREEN}Btop have been installed."
+echo -e "${GREEN}Type ${CYAN}btop${GREEN} to use!"
