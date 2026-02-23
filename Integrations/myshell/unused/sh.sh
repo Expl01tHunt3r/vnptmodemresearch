bb334d53a55f4c29506f36b6ecdd1683edac7a32
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-02-23 17:22:04 +0700

    Update sh.sh

--- Integrations/myshell/unused/sh.sh
+++ Integrations/myshell/unused/sh.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 /tmp/userdata/myshell/config.sh
 exec /bin/sh
+
+
+
+NETINFO=""
+for IF in $(grep ":" /proc/net/dev | awk -F: '{print $1}' | sed 's/ //g' | grep -v "^lo$"); do
+    LINE=$(grep "$IF" /proc/net/dev | awk -F: '{print $2}')
+    RX=$(echo $LINE | awk '{print int($1/1024/1024)}')
+    TX=$(echo $LINE | awk '{print int($9/1024/1024)}')
+
+    if [ "$RX" -ne 0 ] || [ "$TX" -ne 0 ]; then
+        NETINFO="$NETINFO
+$IF: Download ${RX}MB | Upload ${TX}MB"
+    fi
+done
+
+echo "${NETINFO}"
+echo -e ""
+echo -e "${YELLOW}=============================================${RESET}"
