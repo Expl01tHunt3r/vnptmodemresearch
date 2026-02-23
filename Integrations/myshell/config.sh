1da2731fa1e9067fff6f590e87754b8dc68b60f4
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-02-23 13:38:31 +0700

    Update XDG_CONFIG_HOME path for btop configuration

--- Integrations/myshell/config.sh
+++ Integrations/myshell/config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # this for btop, i'm sure it very useful
-export XDG_CONFIG_HOME=/tmp/userdata/btop/
+export XDG_CONFIG_HOME=/tmp/userdata/btop/config
 export PATH=/tmp/userdata/btop:$PATH
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
