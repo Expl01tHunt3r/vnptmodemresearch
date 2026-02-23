deccc9b22dff2c1f1f4f1c146cb867724796e4f9
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-02-23 13:26:35 +0700

    Update dropbear command to use config.sh

--- Integrations/myshell/startup.sh
+++ Integrations/myshell/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 killall dropbear
-/userfs/bin/dropbear -c /tmp/userdata/myshell/sh.sh -b /tmp/userdata/myshell/banner.txt
+/userfs/bin/dropbear -c /tmp/userdata/myshell/config.sh -b /tmp/userdata/myshell/banner.txt
 
