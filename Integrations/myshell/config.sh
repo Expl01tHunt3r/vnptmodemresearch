34fe6f76be91f965bc0ee48093cc5774c1e5c331
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-02-25 14:23:18 +0700

    add show xPON temp and model name

--- Integrations/myshell/config.sh
+++ Integrations/myshell/config.sh
@@ -16,7 +16,14 @@ MEM_USED=$(expr $MEM_TOTAL - $MEM_FREE)
 CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print int(usage)}')
 LOAD=$(awk '{print $1}' /proc/loadavg)
 TEMP=$(/usr/bin/cputemp | awk '{print $3}')
-
+raw=$(/userfs/bin/tcapi get Info_PonPhy Temperature 2>/dev/null)
+[ -z "$raw" ] && exit 1
+PONT=$(awk -v r="$raw" '
+BEGIN {
+    if (r > 32767)
+        r = r - 65536
+    printf "%.2f", r/256
+}')
 
 GREEN='\033[32m'
 RESET='\033[0m'
@@ -25,9 +32,12 @@ CYAN='\033[36m'
 echo -e "${GREEN}Login successfully"
 echo -e "\n"
 echo -e "${YELLOW}=============================================${RESET}"
+echo -e "${GREEN}           $(/userfs/bin/tcapi get DeviceInfo_devParaStatic ModelName)"
+echo -e "${YELLOW}=============================================${RESET}"
 echo -e ""
 echo -e "${CYAN}Uptime:${RESET} $(echo "${H}h ${M}m ${S}s")"
 echo -e "${CYAN}CPU:${RESET} ${CPU}% | ${TEMP}°C"
+echo -e "${CYAN}xPON: ${RESET}${PONT}°C"
 echo -e "${CYAN}Load AVG:${RESET} ${LOAD}"
 echo -e "${CYAN}RAM:${RESET} ${MEM_USED}MB/${MEM_TOTAL}MB"
 echo -e ""
