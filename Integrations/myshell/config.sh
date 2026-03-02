31c2c11fadcc4632c96b977936e039a136f88a2e
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-03-02 14:17:09 +0700

    remove show device connect

--- Integrations/myshell/config.sh
+++ Integrations/myshell/config.sh
@@ -42,43 +42,5 @@ echo -e "${CYAN}Load AVG:${RESET} ${LOAD}"
 echo -e "${CYAN}RAM:${RESET} ${MEM_USED}MB/${MEM_TOTAL}MB"
 echo -e ""
 echo -e "${YELLOW}=============================================${RESET}"
-echo -e ""
-echo -e "${CYAN}List device connecting to your network:${RESET}"
-/userfs/bin/tcapi show DhcpLease 2>/dev/null | awk '
-/\[IP = / {
-    ip=$3
-    gsub(/\]/,"",ip)
-}
-/\[HostName = / {
-    name=$3
-    gsub(/\]/,"",name)
-
-    split(ip, o, ".")
-    key = o[1]*16777216 + o[2]*65536 + o[3]*256 + o[4]
-
-    count++
-    iplist[count] = ip
-    namelist[count] = name
-    keylist[count] = key
-}
-
-END {
-    for (i=1; i<=count; i++) {
-        for (j=i+1; j<=count; j++) {
-            if (keylist[i] > keylist[j]) {
-                tmp=keylist[i]; keylist[i]=keylist[j]; keylist[j]=tmp
-                tmp=iplist[i]; iplist[i]=iplist[j]; iplist[j]=tmp
-                tmp=namelist[i]; namelist[i]=namelist[j]; namelist[j]=tmp
-            }
-        }
-    }
-
-    for (i=1; i<=count; i++) {
-        printf "%-20s %s\n", namelist[i], iplist[i]
-    }
-}
-'
-echo -e ""
-echo -e "${YELLOW}=============================================${RESET}"
 echo -e "\n"
 exec /bin/sh
