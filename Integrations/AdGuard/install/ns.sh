1d86266f209f0cb87a0886b852ddb4dd1b7c8891
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-04-21 22:04:02 +0700

    autostartup power by tcapi

--- Integrations/AdGuard/install/ns.sh
+++ Integrations/AdGuard/install/ns.sh
@@ -13,19 +13,6 @@ YELLOW='\033[33m'
 CYAN='\033[36m'
 BOLD='\033[1m'
 
-install_autorun() {
-    cd /tmp && /userfs/bin/curl -s -k -o autorun.sh https://raw.githubusercontent.com/Expl01tHunt3r/vnptmodemresearch/refs/heads/master/Integrations/autorun/patch.sh && chmod +x autorun.sh && sh autorun.sh
-    cd /tmp/userdata/AdGuard && /userfs/bin/curl -s -k -o startup.sh https://raw.githubusercontent.com/Expl01tHunt3r/vnptmodemresearch/refs/heads/master/Integrations/AdGuard/startup.sh && chmod +x startup.sh
-    echo "sh /tmp/userdata/AdGuard/startup.sh" >> /tmp/userdata/startup.sh
-    echo "Done loading AdGuardHome startup script."
-}
-
-AUTORUN_INSTALLED=true
-echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-
-if [ ! -f /tmp/userdata/startup.sh ]; then
-    AUTORUN_INSTALLED=false
-fi
 
 echo -e "\033[31;43mScript Make By AppleSang With <3\033[0m\n"
 echo -e "${RED}############################################################\n"
@@ -49,22 +36,9 @@ cd AdGuardHome
 chmod +x AdGuardHome
 kill -9 $(pidof dnsmasq)
 IP=$(ip addr show br0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
-if [ "$AUTORUN_INSTALLED" = "false" ]; then
-    echo -e "${YELLOW}############################################################\n" 
-    echo -e "${RESET}Looks like you didn't install our ${CYAN}Autorun${RESET} module yet."
-    echo -e "You will need to login into this shell and start AdGuardHome ${BOLD}every single time${RESET} your router reboots."
-    echo -e "You can see our Autorun here: ${RED}https://github.com/Expl01tHunt3r/vnptmodemresearch/tree/master/Integrations/autorun"
-    echo -e "${YELLOW}############################################################\n"
-    echo -e "${CYAN}Do you want to install Autorun? (${GREEN}Y${CYAN}/${RED}N${CYAN})${RESET}\n"
-    read CONFIRM_AUTORUN_INSTALL
-    case $CONFIRM_AUTORUN_INSTALL in
-        [Yy]* ) install_autorun ;;
-        [Nn]* ) echo "You choosed no. Thanks for your confirmation." ;;
-        * ) echo -e "Default is no.";;
-    esac
-else
-    cd /tmp && /userfs/bin/curl -s -k -o startup.sh https://github.com/Expl01tHunt3r/vnptmodemresearch/raw/refs/heads/master/Integrations/AdGuard/startup.sh && cp startup.sh /tmp/userdata/startup.sh
-fi
+/userfs/bin/tcapi set Autoexec_Entry adguard "sh /tmp/userdata/AdGuard/startup.sh"
+/userfs/bin/tcapi commit Autoexec_Entry
+echo -e "${GREEN}[OK]${RESET} Set auto startup for AdGuardHome."
 /userfs/bin/tcapi set Dhcpd_Entry primary_dns ${IP}
 /userfs/bin/tcapi set Dhcpd_Entry secondary_dns 8.8.8.8
 /userfs/bin/tcapi set Dhcpd_Entry dns_mode 1
