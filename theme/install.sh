e5096300e62bee0c233ca316234bd6c01d4a4a68
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-05-06 12:11:19 +0700

    Update install.sh

--- theme/install.sh
+++ theme/install.sh
@@ -1,1 +1,4 @@
+cd /tmp
 /userfs/bin/curl -s -fSL -o capuchino.zip --retry 1000 --retry-delay 5 https://github.com/Expl01tHunt3r/vnptmodemresearch/raw/refs/heads/master/theme/capuchino.zip
+/etc/safegate/tools/unzip capuchino.zip 
+killall boa
