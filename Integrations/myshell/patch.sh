3f29494332d5e00ccaa64b2dc3ce3158273194df
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-06-23 07:22:47 +0700

    Update patch.sh

--- Integrations/myshell/patch.sh
+++ Integrations/myshell/patch.sh
@@ -13,10 +13,10 @@ RESET='\033[0m'
 YELLOW='\033[33m'
 CYAN='\033[36m'
 # Check req
-if [ ! -f /tmp/userdata/startup.sh ]; then
+if [ ! -f /userfs/bin/dropbear ]; then
     echo -e "\n\n"
     echo -e "${YELLOW}!! ${RED}Can't run this patch ${YELLOW}!!"
-    echo -e "${RED}You must read about this for resolve this issue!"
+    echo -e "${RED}You must read about this for resolve this issue! ${RESET}(${RED}9Error: ${RESET}DropBear not found!)"
     echo -e "${GREEN}https://github.com/Expl01tHunt3r/vnptmodemresearch/tree/master/Integrations/myshell#1-y%C3%AAu-c%E1%BA%A7u${RESET}"
     echo -e "\n\n"
     exit 1
@@ -36,8 +36,10 @@ cd myshell
 /userfs/bin/curl -s -k -o config.sh https://raw.githubusercontent.com/Expl01tHunt3r/vnptmodemresearch/refs/heads/master/Integrations/myshell/config.sh && chmod +x config.sh
 /userfs/bin/curl -s -k -o startup.sh https://raw.githubusercontent.com/Expl01tHunt3r/vnptmodemresearch/refs/heads/master/Integrations/myshell/startup.sh && chmod +x startup.sh
 /userfs/bin/curl -s -k -o banner.txt https://raw.githubusercontent.com/Expl01tHunt3r/vnptmodemresearch/refs/heads/master/Integrations/myshell/banner.txt
-sed -i '\|^sh /tmp/userdata/myshell/startup\.sh$|d' /tmp/userdata/startup.sh
-echo "sh /tmp/userdata/myshell/startup.sh" >> /tmp/userdata/startup.sh
+/userfs/bin/tcapi set Autoexec_Entry myshell "sh /tmp/userdata/myshell/startup.sh"
+/userfs/bin/tcapi commit Autoexec_Entry
+/userfs/bin/tcapi save
+echo -e "${GREEN}[OK]${RESET} Set auto startup for myshell."
 echo -e "${GREEN}Patch completed!${RESET}"
 echo -e "${CYAN}You need open telnet and type ${YELLOW}/tmp/userdata/myshell/startup.sh${RESET}"
 echo -e "${CYAN}Or just restart router to apply change!${RESET}"
